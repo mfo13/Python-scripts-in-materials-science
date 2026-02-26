c6c15ba516346d960562e692e78046fb08d2061c
mfo13 <marcelo.falcao.oliveira@gmail.com>
2026-02-26 08:25:06 -0300

    minor cosmetics
    minor cosmetics

--- hydrogen-orbitals.py
+++ hydrogen-orbitals.py
@@ -11,8 +11,8 @@ Description:
 This Python script show 3D views for the solutions of Schrödinger's equation for hydrogen:
 - 3D wave function
 - 3D probability density
-- Clipping planes of the 3D probability density
-- 3D contours of the probability density (>90%)
+- Cutting planes of the 3D probability density
+- 3D contours of the probability density (>80%)
 - Dot density plot for the 3D proability density 
 
 License:
@@ -27,7 +27,7 @@ argparse, vtk, numpy, sympy, pyvista
 
 Usage:
 $ python hydrogen_orbitals.py <n> <l> <m>
-- <n>: Quantum number n (n >= 1)
+- <n>: Quantum number n (n >= 1), 6 max.
 - <l>: Quantum number l (0 <= l < n)
 - <m>: Quantum number m (-l <= m <= l)
 - Use 'python hydrogen_orbitals.py -h' for help.
@@ -41,12 +41,11 @@ It was also improved with the assistance of ChatGPT-3.5 and Gemini 3
 """
 
 import argparse                     # Importing the argparse module for command-line argument parsing
-import vtk                          # Importing the VTK (Visualization Toolkit) module for 3D computer graphics
+import vtk                          # Importing the VTK (Visualization Toolkit), needed to render latex
 import numpy as np                  # Importing the NumPy library for numerical operations
 import pyvista as pv                # Importing the PyVista library for 3D visualization and analysis
 from sympy.abc import phi, r, theta # Importing specific symbols (phi, r, theta) from the sympy.abc module
 from sympy import latex             # Importing the latex function from the sympy module for LaTeX formatting
-            
 
 def parse_arguments():
     """
@@ -58,7 +57,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Hydrogen Orbitals Viewer.')
     
     # Add command-line arguments for principal, azimuthal, and magnetic quantum numbers
-    parser.add_argument('n', metavar='n', nargs='?', default=4, type=int, help='Principal quantum number n (n >= 1)')
+    parser.add_argument('n', metavar='n', nargs='?', default=4, type=int, help='Principal quantum number n (n >= 1), 6 max.')
     parser.add_argument('l', metavar='l', nargs='?', default=3, type=int, help='Azimuthal quantum number l (l < n)')
     parser.add_argument('m', metavar='m', nargs='?', default=2, type=int, help='Magnetic quantum number m (-l <= m <= l)')
     
@@ -153,7 +152,7 @@ def prob_dens(n=1, l=0, m=0):
     else:
         dens = Abs(psi)         # If m = 0, there is no imaginary part
         
-    # Multiply the absolute value by r^2 if l = 0 since it is a uniform radial distribution (we want see the radial probability in such a case)
+    # Multiply the absolute value by r if l = 0 since it is a uniform radial distribution (we want see the radial probability in such a case)
     if l == 0:
         dens *= r
     
@@ -187,7 +186,7 @@ def plot_slice(orbital, cpos='iso', cut_plane='x', eq=''):
     Returns:
         pyvista.Plotter: PyVista plotter object for visualization.
     """
-    pl = pv.Plotter()
+    pl = pv.Plotter()       # initialize the plotter
      
     # Define scalar bar title based on the azimuthal quantum number (l)
     scalar_bar_title = '$\\mid\\psi\\mid^2r^2$' if int(args.l) == 0 else '$\\mid\\psi\\mid^2$'  # For l=0, include r^2 in the scalar bar title
@@ -201,7 +200,7 @@ def plot_slice(orbital, cpos='iso', cut_plane='x', eq=''):
         orbital, 
         scalars = "probability density", 
         cmap = "rainbow",                   # color map for the probability density
-        generate_triangles = True,
+        generate_triangles = True,          # triangles to fill the cut plane
         normal = cut_plane,                 # cut plane normal axis
         interaction_event = 'always',       # make the widget interactive
         opacity = opacity_mapping ,         # opacity mapping to remove the background
@@ -216,13 +215,12 @@ def plot_slice(orbital, cpos='iso', cut_plane='x', eq=''):
     
     return pl.show()
 
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Execute the main code only if this script is run directly, not when imported as a module
+# Execute the main code only if this script is running directly, not when imported as a module
 if __name__ == "__main__":
 
-    # Parse command line arguments
-    args = parse_arguments()
+    args = parse_arguments()                # Parse command line arguments
 
     # Set PyVista global theme settings
     pv.global_theme.background = 'black'    # Set the plotter background color
@@ -237,16 +235,16 @@ if __name__ == "__main__":
     expr_prob = f'$\\mid\\psi_{{({args.n},{args.l},{args.m})}}\\mid^2r^2=\\mid{expr}\\mid^2r^2$' if args.l == 0 else f'$\\mid\\psi_{{({args.n},{args.l},{args.m})}}\\mid^2=\\mid{expr}\\mid^2$'
 
     # Plot block 1: 3D Wave Function
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     grid = wfc(args.n, args.l, args.m)      # Get the wave function evaluation grid
     pl = pv.Plotter()                       # Initialize the PyVista plotter
 
     # Add a volume to the plotter with the desired grid (orbital) and set additional details below
     vol = pl.add_volume(
         grid,
-        cmap='twilight' if args.n > 1 else 'pink',    # Define the color map
-        opacity=[1, 0, 1] if args.n > 1 else 'linear',  # Define the opacity
-        scalar_bar_args={'title': '$\\psi$', 'use_opacity': False},  # Set the title of the scalar bar and disable opacity 
+        cmap='twilight' if args.n > 1 else 'pink',                  # Define the color map
+        opacity=[1, 0, 1] if args.n > 1 else 'linear',              # Define the opacity
+        scalar_bar_args={'title': '$\\psi$', 'use_opacity': False}  # Set the title of the scalar bar and disable opacity 
     )
     vol.prop.interpolation_type = 'linear'  # Interpolation of the scalar values in between points
 
@@ -255,7 +253,7 @@ if __name__ == "__main__":
         pl.add_volume_clip_plane(
             vol,
             normal='-x',                # x-axis cut by the clipping plane
-            normal_rotation=False      # Avoid rotation of the clipping plane by the user; only translation is possible
+            normal_rotation=False       # Avoid rotation of the clipping plane by the user; only translation is possible
         )
 
     pl.add_text(expr_wfc, font_size=14)     # Add the text of the equation to the plot window
@@ -263,36 +261,31 @@ if __name__ == "__main__":
     pl.show()                               # Show the plot window
     
     # Plot block 2: 3D Orbital Probability Density
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     grid = prob_dens(args.n, args.l, args.m)    # Get the probability density function evaluation grid
     pl = pv.Plotter()                           # Initialize the PyVista plotter
-
-    # Define the title for the scalar bar
-    title_bar = '$\\mid\\psi\\mid^2r^2$' if args.l == 0 else '$\\mid\\psi\\mid^2$'
+    title_bar = '$\\mid\\psi\\mid^2r^2$' if args.l == 0 else '$\\mid\\psi\\mid^2$'  # Define the title for the scalar bar
 
     # Add a volume to the plotter with the desired grid (orbital) and set additional details below
     vol = pl.add_volume(
         grid,
-        cmap='rainbow',  # Define the color map for the probability density
-        scalar_bar_args={'title': title_bar, 'use_opacity': False},  # Set the title of the scalar bar and disable opacity
+        cmap='rainbow',                                             # Define the color map for the probability density
+        scalar_bar_args={'title': title_bar, 'use_opacity': False}  # Set the title of the scalar bar and disable opacity
     )
-    vol.prop.interpolation_type = 'linear'  # Interpolation of the scalar values in between points
 
-    pl.add_text(expr_prob, font_size=14)  # Add the text of the equation to the plot window
-    pl.show_axes()  # Make x, y, and z axes visible
-    pl.show()   # Show the plot window
+    vol.prop.interpolation_type = 'linear'  # Interpolation of the scalar values in between points
+    pl.add_text(expr_prob, font_size=14)    # Add the text of the equation to the plot window
+    pl.show_axes()                          # Make x, y, and z axes visible
+    pl.show()                               # Show the plot window
 
     # Plot block 3: Cutting Planes of the 3D Probability Density 
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    #clip plane at x=0 
-    plot_slice(grid, cut_plane='x', eq=expr_prob)
-    # clip plane at y=0
-    plot_slice(grid, cut_plane='y', eq=expr_prob)
-    # clip plane at z=0
-    plot_slice(grid, cut_plane='z', eq=expr_prob)
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    plot_slice(grid, cut_plane='x', eq=expr_prob)   # clip plane at x=0
+    plot_slice(grid, cut_plane='y', eq=expr_prob)   # clip plane at y=0
+    plot_slice(grid, cut_plane='z', eq=expr_prob)   # clip plane at z=0
 
     # Plot block 4: Orbital Contours as an Isosurface
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     pl = pv.Plotter()
     probability = 80                                        # define the probability
     grid = prob_dens(args.n, args.l, args.m)                # Get the probability density function evaluation grid
@@ -302,42 +295,46 @@ if __name__ == "__main__":
     target_prob = probability/100 * cumulative_sum[-1]      # define the probability target
     index = np.where(cumulative_sum >= target_prob)[0][0]   # find the cut-off index
     eval_at = data_sorted[index]
+    
     # Make the contour
     contours = grid.contour(
         [eval_at],                  # Contour values (just one here)
         method='marching_cubes'     # VTK filter to create the contour
     )
     contours = contours.interpolate(grid)   # Interpolate the countour with the grid
+
+    # produce the mesh points
     pl.add_mesh(
         contours, 
-        smooth_shading=True, 
-        show_scalar_bar=False, 
-        color='blue', 
-        opacity=0.5
+        smooth_shading=True,    # smooth surface for the countour    
+        show_scalar_bar=False,  # no scale bar needed
+        color='blue',           # color of the surface countour
+        opacity=0.5             # the surface is transparent, you can see through
     )
-    # Adiciona o texto corretamente com os parâmetros de fonte
-    pl.add_text(f'Volume with {probability}% of probability.', font_size=11)
-    pl.show_axes()
-    pl.show()
+    pl.add_text(f'Volume with {probability}% of probability.', font_size=11)    # title
+    pl.show_axes()                                                              # show x, y and z axes
+    pl.show()                                                                   # show the plot
     
     # Plot block 5: Orbital Probability Density with a Density Plot of Points
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     pl = pv.Plotter()
     n_points = 10000                                                        # number of points
     prob = grid['probability density'] / grid['probability density'].sum()  # normalize the probability
     indices = np.random.choice(grid.n_points, n_points, p=prob)             # sort applying the probability
-    points = grid.points[indices].copy()
+    points = grid.points[indices].copy()                                    # copy the points to handle them
     points += (np.random.rand(points.shape[0], 3) - 0.5) * grid.spacing     # add some noise
     point_cloud = pv.PolyData(points)                                       # discret points object
+    
+    # build the mesh points
     pl.add_mesh(
         point_cloud,
-        style='points_gaussian',
-        render_points_as_spheres=False,
-        point_size=0.7,                 
-        emissive=False,
-        show_scalar_bar=False,
-        color=[0, 200, 255]             # cian
+        style='points_gaussian',        # points are not sharp
+        render_points_as_spheres=False, # points are dots
+        point_size=0.7,                 # size of the gaussian dots
+        emissive=False,                 # points are just reflective
+        show_scalar_bar=False,          # no scale bar is needed
+        color=[0, 200, 255]             # cian to better view
     )
     pl.add_text(f'Cloud with {n_points} points.', font_size=11) # title
-    pl.show_axes()
-    pl.show()
+    pl.show_axes()                                              # show x, y and z axes
+    pl.show()                                                   # show the plot
